49417cb409552c17bd6655e57dc66f71c68ec50c
pl <0x00.pl@gmail.com>
2017-06-22 15:00:52 +0800

    use cache on local

--- install-base-env.sh
+++ install-base-env.sh
@@ -1,8 +1,8 @@
 mv /etc/apt/sources.list /etc/apt/sources.list.back
 mv /etc/apt/sources.list.d /etc/apt/sources.list.d.back
 cat > /etc/apt/sources.list << EOF
-deb http://archive.raspbian.org/raspbian stable main contrib non-free
-deb-src http://archive.raspbian.org/raspbian stable main contrib non-free
+deb http://zhiliao-pi.local:3142/archive.raspbian.org/raspbian stable main contrib non-free
+deb-src http://zhiliao-pi.local:3142/archive.raspbian.org/raspbian stable main contrib non-free
 EOF
 
 wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -

1e7ce70179ab4e05027cb75822a1aca636d452ab
pl <0x00.pl@gmail.com>
2017-06-22 16:45:08 +0800

    fix

--- install-base-env.sh
+++ install-base-env.sh
@@ -1,8 +1,8 @@
 mv /etc/apt/sources.list /etc/apt/sources.list.back
 mv /etc/apt/sources.list.d /etc/apt/sources.list.d.back
 cat > /etc/apt/sources.list << EOF
-deb http://zhiliao-pi.local:3142/archive.raspbian.org/raspbian stable main contrib non-free
-deb-src http://zhiliao-pi.local:3142/archive.raspbian.org/raspbian stable main contrib non-free
+deb https://mirrors.ustc.edu.cn/raspbian/raspbian stable main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/raspbian/raspbian stable main contrib non-free
 EOF
 
 wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -

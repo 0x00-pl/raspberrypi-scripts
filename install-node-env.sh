8348d5eeb275179781aefe03327ad3de28291423
0x00-pl <0x00.pl@gmail.com>
2017-07-31 03:01:50 +0000

    add patch openresolv

--- install-node-env.sh
+++ install-node-env.sh
@@ -1,2 +1,2 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y --force-yes nodejs
+apt-get install -y nodejs npm
---
+++ patch_openresolv/patch.sh
@@ -0,0 +1,4 @@
+sudo cp -r etc /
+sudo cp -r lib /
+
+

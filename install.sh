77798dd941eae264f5d10b44a3c06b6d6e5d302f
pl <0x00.pl@gmail.com>
2017-08-03 10:39:41 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd `dirname $0`
+
+sodu sh enable_root_ssh.sh
+sudo sh install-base-env.sh
+sudo sh install-tools.sh
+sudo sh install-node-env.sh
+sudo sh setup_network/setup_wifi.sh
+sudo sh setup_network/enable_ip_forward.sh
+sudo sh patch_openresolv/patch.sh

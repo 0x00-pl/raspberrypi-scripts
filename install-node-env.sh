87c0bec1f9f12a11f058b4b522658cbac72f7f3f
pl <0x00.pl@gmail.com>
2017-08-07 17:21:47 +0800

    change scripts

--- install-node-env.sh
+++ install-node-env.sh
@@ -1,5 +1,5 @@
 mkdir -p /etc/apt/sources.list.d
-rm /etc/apt/sources.list.d/nodesource.list
+rm -f  /etc/apt/sources.list.d/nodesource.list
 
 #curl https://deb.nodesource.com/setup_8.x | sudo -E bash -
 cat proxychains_setup_8.x | sudo -E bash -

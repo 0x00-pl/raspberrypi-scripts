2b860e87caf98f55cdd2e0797f44d83734e10c3c
pl <0x00.pl@gmail.com>
2017-08-07 15:59:46 +0800

    change script

--- install-node-env.sh
+++ install-node-env.sh
@@ -1,6 +1,6 @@
 mkdir -p /etc/apt/sources.list.d
 rm /etc/apt/sources.list.d/nodesource.list
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+cat setup_8.x | sudo -E bash -
 
 apt-get install -y nodejs
 

b8015217e2c5049a6d4cb13bf060c4acaef0aaaf
pl <0x00.pl@gmail.com>
2017-08-29 15:48:37 +0800

    fix install script

--- install-node-env.sh
+++ install-node-env.sh
@@ -4,6 +4,6 @@ rm -f  /etc/apt/sources.list.d/nodesource.list
 #curl https://deb.nodesource.com/setup_8.x | sudo -E bash -
 cat proxychains_setup_8.x | sudo -E bash -
 
-sudo apt-get install -y nodejs
+sudo apt-get install -y --allow-unauthenticated nodejs
 
 sudo npm install -g pm2
